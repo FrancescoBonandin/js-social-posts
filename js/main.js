7237f58a64737f40cc1cc42e3ca612f72eaa6fa4
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-06-28 16:36:41 +0200

    EDIT MS2 completata

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+// 1. ciclo array e creo gli elementi che mi servono, gli assegno le classi e li riempio
+
+
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +59,115 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+const container = document.getElementById("container")
+
+for (let index = 0; index < posts.length; index++) {
+    const element = posts[index];
+    const creationDateObj = dateToDateObj(element.created);
+    const dateUSA = `${creationDateObj.month}/${creationDateObj.day}/${creationDateObj.year}`;
+
+
+
+    container.innerHTML += `<div class="post">
+
+            <div class="post__header">
+
+                <div class="post-meta">   
+
+                    <div class="post-meta__icon">
+
+                        <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
+                   
+                    </div>
+
+                    <div class="post-meta__data">
+
+                        <div class="post-meta__author">${element.author.name}</div>
+
+                        <div class="post-meta__time">${dateUSA}</div>
+
+                    </div>       
+
+                </div>
+
+            </div>
+
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            
+            <div class="post__image">
+
+                <img src="${element.media}" alt="">
+
+            </div>
+
+            <div class="post__footer">
+
+                <div class="likes js-likes">
+
+                    <div class="likes__cta">
+
+                        <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
+
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+
+                            <span class="like-button__label">Mi Piace</span>
+
+                        </a>
+
+                    </div>
+
+                    <div class="likes__counter">
+
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+
+                    </div>
+
+                </div>
+                 
+            </div>      
+
+        </div>`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function dateToDateObj(date){
+
+    let arr = date.split("-");
+
+    let dateObj = {
+
+        year:undefined,
+        month:undefined,
+        day:undefined
+
+    };
+
+    for (let index = 0; index < arr.length; index++) {
+
+        const element = arr[index];
+
+        for (const key in dateObj) {
+
+            dateObj[key] = element;
+         
+        }
+        
+    }
+
+    return dateObj;
+}
